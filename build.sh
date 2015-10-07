328e7f4ff699f9c5a3854533a92c63a9eea481cd
Chaoxu Tong <blackchaoxu@gmail.com>
2015-10-07 11:57:17 -0400

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 (cd frontend && npm run build)
-mkdir -p backend/server/src/main/resources/public
-cp -r frontend/index.html frontend/static backend/server/src/main/resources/public
+mkdir -p backend/server/src/main/resources/public/static
+cp frontend/index.html backend/server/src/main/resources/public
+cp frontend/static/bundle.js backend/server/src/main/resources/public/static
 (cd backend/server && gradle installDist)
 # docker build -t tongcx/schedule-demo .

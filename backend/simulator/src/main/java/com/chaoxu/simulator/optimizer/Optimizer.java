4ebe65eb0155e015ecf698f5294529f209928a15
Chaoxu Tong <blackchaoxu@gmail.com>
2015-10-14 21:45:15 -0400

    make sure diverted patients are better off

--- backend/simulator/src/main/java/com/chaoxu/simulator/optimizer/Optimizer.java
+++ backend/simulator/src/main/java/com/chaoxu/simulator/optimizer/Optimizer.java
@@ -14,10 +14,12 @@ import com.chaoxu.simulator.Evaluator;
 
 class Result {
     public SummaryStatistics stat;
+    public SummaryStatistics patientStat;
     public String site;
 }
 
 public class Optimizer {
+    // TODO make this a configuration
     private static final double confidenceLevel = 2.575;
 
     public static Result bestSite(State state,
@@ -26,18 +28,26 @@ public class Optimizer {
 
         String originalSite = patient.site;
         Map<String, SummaryStatistics> objDiff = new HashMap<>();
+        Map<String, SummaryStatistics> patientDiff = new HashMap<>();
 
         for (String site : state.sites.keySet()) {
-            if (site != originalSite) {
+            if (!site.equals(originalSite)) {
                 SummaryStatistics stat = new SummaryStatistics();
+                SummaryStatistics patientStat = new SummaryStatistics();
                 for (RandomBits bits : lBits) {
-                    double obj = objective.value(Evaluator.evaluate(state, bits));
+                    Map<String, Double> wait = Evaluator.evaluate(state, bits);
+                    double obj = objective.value(wait);
+
                     patient.site = site;
-                    double newObj = objective.value(Evaluator.evaluate(state, bits));
+                    Map<String, Double> newWait = Evaluator.evaluate(state, bits);
+                    double newObj = objective.value(newWait);
                     patient.site = originalSite;
+
                     stat.addValue(newObj - obj);
+                    patientStat.addValue(newWait.get(patient.name) - wait.get(patient.name));
                 }
                 objDiff.put(site, stat);
+                patientDiff.put(site, patientStat);
             }
         }
 
@@ -53,9 +63,17 @@ public class Optimizer {
             double delta = stat.getStandardDeviation() * confidenceLevel / Math.sqrt(stat.getN());
             if (mean + delta >= 0) continue;
 
+            SummaryStatistics patientStat = patientDiff.get(site);
+            // make sure the patient in question is better off
+            // TODO make this a config
+            if (patientStat.getMean() +
+                    patientStat.getStandardDeviation() * confidenceLevel
+                    / Math.sqrt(patientStat.getN()) >= 0) continue;
+
             if (mean < best.stat.getMean()) {
                 best.site = site;
                 best.stat = stat;
+                best.patientStat = patientStat;
             }
         }
 
@@ -66,10 +84,16 @@ public class Optimizer {
             List<RandomBits> lBits) {
         Result result = bestSite(state, patient, lBits);
 
-        if (result.site != patient.site) {
+        if (!result.site.equals(patient.site)) {
             SummaryStatistics stat = result.stat;
 
             System.out.println();
+            System.out.println(String.format(
+                        "%s mean %f sd %f delta %f",
+                        patient, stat.getMean(), stat.getStandardDeviation(),
+                        stat.getStandardDeviation() * confidenceLevel / Math.sqrt(stat.getN())));
+
+            stat = result.patientStat;
             System.out.println(String.format(
                         "%s mean %f sd %f delta %f",
                         patient, stat.getMean(), stat.getStandardDeviation(),
